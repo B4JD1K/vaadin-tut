0031a2e5484e31474f9db7ebe1ca38609714ff54
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2023-06-05 20:17:07 +0200

    6 part finished

---
+++ src/main/java/com/example/application/data/entity/Contact.java
@@ -0,0 +1,85 @@
+package com.example.application.data.entity;
+
+//import com.example.application.data.AbstractEntity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+//@Entity
+public class Contact extends AbstractEntity {
+//    @Id
+    @NotEmpty
+    private String firstName = "";
+
+    @NotEmpty
+    private String lastName = "";
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    @NotNull //    @NotEmpty
+    @JsonIgnoreProperties({"employees"})
+    private  Company company;
+
+    /**
+     *      @NotNull    - do combobox, żeby wybrać jakąś opcję
+     *      @NotEmpty   - do textfieldów, żeby wpisać jakąś wartość
+     */
+
+    @NotNull//    @NotEmpty
+    @ManyToOne
+    private Status status;
+
+    @Email
+    @NotEmpty
+    private String email = "";
+
+    @Override
+    public String toString() {
+        return firstName+" "+lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
